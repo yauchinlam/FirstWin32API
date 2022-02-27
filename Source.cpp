29dad4e8137c847c89bdd2131d74e0cab78994ee
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-02-27 00:03:07 -0500

    Added edit controls and static controls

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,15 @@
 #define FILE_MENU_NEW 1
 #define FILE_MENU_OPEN 2
 #define FILE_MENU_EXIT 3
+#define CHANGE_TITLE 4
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
 void AddMenus(HWND);
+void AddControls(HWND);
 
 HMENU hMenu;
+HWND hEdit;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
@@ -50,10 +53,16 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 		case FILE_MENU_NEW:
 			MessageBeep(MB_ICONINFORMATION);
 			break;
+		case CHANGE_TITLE:
+			wchar_t text[100];
+			GetWindowTextW(hEdit, text, 100);
+			SetWindowTextW(hWnd, text);
+			break;
 		}
 		break;
 	case WM_CREATE:
 		AddMenus(hWnd);
+		AddControls(hWnd);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -69,7 +78,7 @@ void AddMenus(HWND hWnd)
 	HMENU hFileMenu = CreateMenu();
 	HMENU hSubMenu = CreateMenu();
 
-	AppendMenuA(hSubMenu, MF_STRING, NULL, "SubMenu Item");
+	AppendMenuA(hSubMenu, MF_STRING, CHANGE_TITLE, "Change Title");
 
 	AppendMenuA(hFileMenu, MF_STRING, FILE_MENU_NEW, "New");
 	AppendMenuA(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, "Open SubMenu");
@@ -80,4 +89,13 @@ void AddMenus(HWND hWnd)
 	AppendMenuA(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, "File");
 	AppendMenuA(hMenu, MF_STRING, NULL, "Help");
 	SetMenu(hWnd, hMenu);
+}
+
+void AddControls(HWND hWnd)
+{
+	CreateWindowW(L"static",L"Enter Text Here: ", WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER, 200, 100, 100, 50, hWnd, 
+		NULL, NULL, NULL);
+
+	hEdit = CreateWindowW(L"Edit", L"...", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 200, 152, 100, 50, hWnd,
+		NULL, NULL, NULL);
 }
